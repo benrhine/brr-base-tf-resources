292e1468edbf1c2dafab252c231d1096eff57d9c
Ben Rhine <rhine.ben@gmail.com>
2025-12-31 12:12:41 -0700

    initial commit

---
+++ aws/tools/account-resources/main.tf
@@ -0,0 +1,115 @@
+
+########################################################################################################################
+# S3 Module Declaration: This module is used to create S3 buckets. In the case of this application this declaration is
+# being used to create the default "deployment_bucket" which will contain all generated application artifacts from other
+# application deployments.
+#
+# NOTE: THIS MODULE DOES NOT HAVE ANY DEPENDENCIES!!!
+# NOTE2: THIS MODULE SHOULD BE REUSABLE
+# WARNING!!! There does not seem to be a way to set the deletion policy in terraform meaning using the destroy command
+# WILL DELETE THE BUCKET!!!
+########################################################################################################################
+# Create a random value to add to the S3 bucket to guarantee uniqueness
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false # Exclude uppercase characters
+  # numeric = false # Exclude numeric characters
+}
+
+locals {
+  convert_to_underscores = replace(var.git_repo_name, "-", "_")
+}
+
+module "s3_tf_state_bucket" {
+  source                        = "git::https://github.com/benrhine/brr-s3-module.git?ref=v0.0.1.7"                                          # Where to find the module
+  ######################################################################################################################
+  #   aws_region                    = data.aws_region.current.name                            # Value retrieved in data.tf
+  #   aws_account                   = data.aws_caller_identity.current.account_id             # Value retrieved in data.tf
+  #   project_name                  = var.project_name                                        # Value passed in via variables.tf
+  # Custom defined value
+  create_bucket_name            = "${var.framework_prefix}-state-${random_string.suffix.result}"
+  s3_tags_environment           = var.tag_environment_tools                                 # Value passed in via variables.tf
+  s3_tags_origination           = var.tag_origination_repo
+  s3_tags_project               = var.project_name
+}
+
+# Enable versioning on bucket that maintains terraform state
+resource "aws_s3_bucket_versioning" "versioning_tf_state" {
+  bucket = module.s3_tf_state_bucket.aws_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  # GitHub OIDC root CA thumbprint - second value may be unnecessary
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+}
+
+# Create an IAM role to be assumed by GitHub Action
+module "github_oidc_ci_assume_role" {
+  source = "git::git@github.com:benrhine/brr-iam-roles-module.git?ref=v0.0.1.1" # Where to find the module
+  ######################################################################################################################   # Value passed in via variables.tf
+  # iam_role_name            = "${var.iam_role_name}_${local.convert_to_underscores}"
+  iam_role_name            = "${var.iam_role_name}"
+  iam_role_description     = "This is the base CI role that will be assumed"
+  iam_assume_role_policy   = data.aws_iam_policy_document.github_oidc_ci_assume_role.json
+  iam_tags_environment     = var.tag_environment
+  iam_tags_origination     = var.tag_origination_repo
+  iam_tags_project         = var.project_name
+}
+
+# Attach IAM Policy to newly created role
+resource "aws_iam_role_policy_attachment" "github_oidc_ci_assume_role_attachment" {
+  role       = module.github_oidc_ci_assume_role.created_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+
+  depends_on = [
+    module.github_oidc_ci_assume_role
+  ]
+}
+
+########################################################################################################################
+# Store values for CI Actions in GitHub: This is necessary so that the AWS Role can be assumed at execution time
+########################################################################################################################
+resource "github_actions_secret" "env_secret_1" {
+  repository = var.git_repo_name
+  # environment     = var.current_env
+  secret_name     = "${upper(var.current_env)}_ROLE"
+  plaintext_value = module.github_oidc_ci_assume_role.created_role_arn
+}
+
+resource "github_actions_secret" "env_secret_2" {
+  repository = var.git_repo_name
+  # environment     = var.current_env
+  secret_name     = "${upper(var.current_env)}_ROLE_DURATION"
+  plaintext_value = var.git_role_duration
+}
+
+resource "github_actions_secret" "env_secret_3" {
+  repository = var.git_repo_name
+  # environment     = var.current_env
+  secret_name     = "${upper(var.current_env)}_AWS_REGION"
+  plaintext_value = var.git_aws_region
+}
+
+########################################################################################################################
+# Store role for CI Actions in Aws: This is to ensure role is accessible for future actions
+########################################################################################################################
+module "ci_role_to_assume" {
+  source = "git::git@github.com:benrhine/brr-ssm-module.git?ref=v0.0.1.1" # Where to find the module
+  ######################################################################################################################
+  property_name             = "/${var.business_area_name}/${lower(var.team_name)}/${lower(var.current_env)}/${var.framework_prefix}/${var.iam_role_name}_${local.convert_to_underscores}" # Custom defined value
+  property_description      = "Role to assume during CI jobs"                                                                                                               # Custom defined value
+  property_value            = module.github_oidc_ci_assume_role.created_role_arn                                                                                            # Value retrieved from module outputs.tf
+  property_type             = "SecureString"
+  property_tags_environment = var.tag_environment # Value passed in via variables.tf
+  property_tags_origination = var.tag_origination_repo
+  property_tags_project     = var.project_name
+}
---
+++ aws/tools/account-resources/outputs.tf
@@ -0,0 +1,365 @@
+########################################################################################################################
+# To view module outputs at runtime, outputs must be re-declared in root module outputs.tf to connect them to sub-modules
+# outputs.tf file
+########################################################################################################################
+
+########################################################################################################################
+# S3 Outputs
+########################################################################################################################
+output "aws_s3_bucket_name" {
+  description = "Base service role name"
+  value       = module.s3_tf_state_bucket.aws_s3_bucket_name
+}
+
+output "aws_s3_bucket_arn" {
+  description = "Base service role ARN"
+  value       = module.s3_tf_state_bucket.aws_s3_bucket_arn
+}
+
+########################################################################################################################
+# S3 Policy Outputs
+########################################################################################################################
+# output "aws_s3_bucket_policy" {
+#   description = "Base service role id"
+#   value       = module.s3_policies.aws_s3_bucket_policy.bucket
+# }
+
+
+output "github_oidc_connection_arn" {
+  description = "ARN of GitHub OIDC Connection"
+  value       = aws_iam_openid_connect_provider.github.arn
+}
+########################################################################################################################
+# SSM Property Outputs
+########################################################################################################################
+# output "created_ssm_properties" {
+#   description = "System manager properties (SSM) created"
+#   value = [for system_property in module.ssm : system_property.aws_ssm_property_arn]
+# }
+
+# output "aws_ssm_property_1_arn" {
+#   description = "Created system manager property arn"
+#   value       = module.ssm_1.aws_ssm_property_arn
+# }
+#
+# output "aws_ssm_property_2_arn" {
+#   description = "Created system manager property arn"
+#   value       = module.ssm_2.aws_ssm_property_arn
+# }
+
+# output "aws_ssm_property_3" {
+#   description = "Created system manager property arn"
+#   value       = data.aws_ssm_parameter.external_service_role_principals.value
+#   sensitive = true
+# }
+#
+# output "aws_ssm_property_4" {
+#   description = "Created system manager property arn"
+#   value       = local.principals
+#   sensitive = true
+# }
+
+########################################################################################################################
+# IAM Roles Outputs
+########################################################################################################################
+# output "create_base_assumed_service_role_name" {
+#   description = "Base role to be assumed name"
+#   value       = module.base_assumed_service_role.created_role_name
+# }
+#
+# output "create_base_assumed_service_role_arn" {
+#   description = "Base role to be assumed ARN"
+#   value       = module.base_assumed_service_role.created_role_arn
+# }
+#
+# output "create_base_service_role_name" {
+#   description = "Base service role name"
+#   value       = module.base_service_role.created_role_name
+# }
+#
+# output "create_base_service_role_arn" {
+#   description = "Base service role ARN"
+#   value       = module.base_service_role.created_role_arn
+# }
+#
+# output "create_codebuild_service_role_name" {
+#   description = "CodeBuild service role name"
+#   value       = module.codebuild_service_role.created_role_name
+# }
+#
+# output "create_codebuild_service_role_arn" {
+#   description = "CodeBuild service role ARN"
+#   value       = module.codebuild_service_role.created_role_arn
+# }
+#
+# output "create_codepipeline_service_role_name" {
+#   description = "CodePipeline service role name"
+#   value       = module.codepipeline_service_role.created_role_name
+# }
+#
+# output "create_codepipeline_service_role_arn" {
+#   description = "CodePipeline service role ARN"
+#   value       = module.codepipeline_service_role.created_role_arn
+# }
+#
+# output "create_codepipeline_event_rule_role_name" {
+#   description = "CodePipeline service role name"
+#   value       = module.codepipeline_event_rule_role.created_role_name
+# }
+#
+# output "create_codepipeline_event_rule_role_arn" {
+#   description = "CodePipeline service role ARN"
+#   value       = module.codepipeline_event_rule_role.created_role_arn
+# }
+#
+# output "create_external_non_prod_service_role_name" {
+#   description = "External role to be assumed at deployment time name"
+#   value       = module.external_non_prod_service_role.created_role_name
+# }
+#
+# output "create_external_non_prod_service_role_arn" {
+#   description = "External role to be assumed at deployment time ARN"
+#   value       = module.external_non_prod_service_role.created_role_arn
+# }
+
+# UNCOMMENT WHEN A PROD ACCOUNT IS AVAILABLE
+# output "create_external_prod_service_role_name" {
+#   description = "External role to be assumed at deployment time name"
+#   value       = module.external_prod_service_role.created_role_name
+# }
+#
+# output "create_external_prod_service_role_arn" {
+#   description = "External role to be assumed at deployment time ARN"
+#   value       = module.external_prod_service_role.created_role_arn
+# }
+
+########################################################################################################################
+# IAM Policy Outputs
+########################################################################################################################
+# output "created_iam_policies" {
+#   description = "IAM policies created"
+#   value = [for policy in module.iam_policies : policy.tf_policy_arn]
+# }
+#
+# output "created_iam_external_non_prod_policies" {
+#   description = "External IAM policies created"
+#   value = [for policy in module.iam_external_non_prod_policies : policy.tf_policy_arn]
+# }
+
+# UNCOMMENT WHEN A PROD ACCOUNT IS AVAILABLE
+# output "created_iam_external_prod_policies" {
+#   description = "External IAM policies created"
+#   value = [for policy in module.iam_external_prod_policies : policy.tf_policy_arn]
+# }
+
+########################################################################################################################
+# IAM Policy Attachments Outputs - Not sure that putting this out would have any value
+########################################################################################################################
+# output "assumed_service_role_policy_attachment_1" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.assumed_service_role_policy_attachment_1
+# }
+#
+# output "assumed_service_role_policy_attachment_2" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.assumed_service_role_policy_attachment_2
+# }
+#
+# output "base_service_role_policy_attachment_1" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.base_service_role_policy_attachment_1
+# }
+#
+# output "base_service_role_policy_attachment_2" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.base_service_role_policy_attachment_2
+# }
+#
+# output "base_service_role_policy_attachment_3" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.base_service_role_policy_attachment_3
+# }
+#
+# output "base_service_role_policy_attachment_4" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.base_service_role_policy_attachment_4
+# }
+#
+# output "base_service_role_policy_attachment_5" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.base_service_role_policy_attachment_5
+# }
+#
+# output "codebuild_service_role_policy_attachment_1" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_1
+# }
+#
+# output "codebuild_service_role_policy_attachment_2" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_2
+# }
+#
+# output "codebuild_service_role_policy_attachment_3" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_3
+# }
+#
+# output "codebuild_service_role_policy_attachment_4" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_4
+# }
+#
+# output "codebuild_service_role_policy_attachment_5" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_5
+# }
+#
+# output "codebuild_service_role_policy_attachment_6" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_6
+# }
+#
+# output "codebuild_service_role_policy_attachment_7" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_7
+# }
+#
+# output "codebuild_service_role_policy_attachment_8" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_8
+# }
+#
+# output "codebuild_service_role_policy_attachment_9" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codebuild_service_role_policy_attachment_9
+# }
+#
+# output "codepipeline_service_role_policy_attachment_1" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codepipeline_service_role_policy_attachment_1
+# }
+#
+# output "codepipeline_service_role_policy_attachment_2" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codepipeline_service_role_policy_attachment_2
+# }
+#
+# output "codepipeline_service_role_policy_attachment_3" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codepipeline_service_role_policy_attachment_3
+# }
+#
+# output "codepipeline_event_rule_role_policy_attachment_1" {
+#   description = "ARN of successful policy attachment"
+#   value       = module.iam_policy_attachments.codepipeline_event_rule_role_policy_attachment_1
+# }
+
+########################################################################################################################
+# CodeBuild Outputs
+########################################################################################################################
+# output "aws_codebuild_account_resources_disaster_recovery_name" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_disaster_recovery.aws_codebuild_project_name
+# }
+#
+# output "aws_codebuild_account_resources_disaster_recovery_arn" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_disaster_recovery.aws_codebuild_project_arn
+# }
+#
+# output "aws_codebuild_account_resources_properties_local_deploy_name" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_properties_deploy.aws_codebuild_project_name
+# }
+#
+# output "aws_codebuild_account_resources_properties_local_deploy_arn" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_properties_deploy.aws_codebuild_project_arn
+# }
+#
+# output "aws_codebuild_account_resources_properties_local_remove_name" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_properties_remove.aws_codebuild_project_name
+# }
+#
+# output "aws_codebuild_account_resources_properties_local_remove_arn" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_properties_remove.aws_codebuild_project_arn
+# }
+#
+# output "aws_codebuild_account_resources_chatbot_cross_account_deploy_name" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_chatbot_cross_account_deploy.aws_codebuild_project_name
+# }
+#
+# output "aws_codebuild_account_resources_chatbot_cross_account_deploy_arn" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_chatbot_cross_account_deploy.aws_codebuild_project_arn
+# }
+#
+# output "aws_codebuild_account_resources_chatbot_cross_account_remove_name" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_chatbot_cross_account_remove.aws_codebuild_project_name
+# }
+#
+# output "aws_codebuild_account_resources_chatbot_cross_account_remove_arn" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_chatbot_cross_account_remove.aws_codebuild_project_arn
+# }
+#
+# output "aws_codebuild_account_resources_repo_notifications_local_deploy_name" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_repo_notifications_deploy.aws_codebuild_project_name
+# }
+#
+# output "aws_codebuild_account_resources_repo_notifications_local_deploy_arn" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_repo_notifications_deploy.aws_codebuild_project_arn
+# }
+#
+# output "aws_codebuild_account_resources_repo_notifications_local_remove_name" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_repo_notifications_remove.aws_codebuild_project_name
+# }
+#
+# output "aws_codebuild_account_resources_repo_notifications_local_remove_arn" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.build_account_resources_repo_notifications_remove.aws_codebuild_project_arn
+# }
+
+# ########################################################################################################################
+# # CodePipeline Outputs
+# ########################################################################################################################
+# output "aws_codepipeline_project_name" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.codepipeline.aws_codepipeline_project_name
+# }
+#
+# output "aws_codepipeline_project_arn" {
+#   description = "Returning CodeBuild Project"
+#   value       = module.codepipeline.aws_codepipeline_project_arn
+# }
+#
+# ########################################################################################################################
+# # Event Rule Outputs
+# ########################################################################################################################
+# output "event_rule_name" {
+#   description = "Event rule name"
+#   value       = module.event_rule.event_rule_name
+# }
+#
+# output "event_rule_arn" {
+#   description = "ARN of created event rule"
+#   value       = module.event_rule.event_rule_arn
+# }
+#
+# output "event_target_target_id" {
+#   description = "ARN of event rule target attachment"
+#   value       = module.event_rule.event_target_target_id
+# }
+#
+# output "event_target_arn" {
+#   description = "ARN of event rule target attachment"
+#   value       = module.event_rule.event_target_arn
+# }
