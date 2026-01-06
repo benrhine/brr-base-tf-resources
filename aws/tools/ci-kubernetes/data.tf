715af8f570d9a3314cc0410799a5e056646e2abf
Ben Rhine <rhine.ben@gmail.com>
2026-01-06 14:03:07 -0700

    adding in ci eks project

---
+++ aws/tools/ci-kubernetes/data.tf
@@ -0,0 +1,12 @@
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {}
+
+
+data "aws_eks_cluster" "eks_cluster" {
+  name = "my-eks-cluster-example-1-QExv89SE"
+}
+
+data "aws_eks_cluster_auth" "eks_cluster" {
+  name = "my-eks-cluster-example-1-QExv89SE"
+}
