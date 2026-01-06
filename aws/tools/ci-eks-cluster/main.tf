a14c0f9a28380234303fa4d92466436da6d595a5
Ben Rhine <rhine.ben@gmail.com>
2026-01-05 17:01:00 -0700

    adding in ci eks project

--- aws/tools/ci-eks-cluster/main.tf
+++ aws/tools/ci-eks-cluster/main.tf
@@ -94,7 +94,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = "my-eks-cluster-example-1-${random_string.suffix.result}"
   role_arn = aws_iam_role.eks_role.arn
   vpc_config {
-    subnet_ids = data.aws_subnet.private[*].id
+    subnet_ids = data.aws_subnets.private[*].id
     security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
   depends_on = [aws_iam_role_policy_attachment.eks_policy]
@@ -104,7 +104,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "my-node-group-example-1-${random_string.suffix.result}"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids      = data.aws_subnet.private[*].id
+  subnet_ids      = data.aws_subnets.private[*].id
   scaling_config {
     desired_size = 1
     max_size     = 2
