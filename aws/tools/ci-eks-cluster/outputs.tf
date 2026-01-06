827f568b8470954f8c34afc670d68e0a534bcb63
Ben Rhine <rhine.ben@gmail.com>
2026-01-06 13:25:53 -0700

    adding in ci eks project

--- aws/tools/ci-eks-cluster/outputs.tf
+++ aws/tools/ci-eks-cluster/outputs.tf
@@ -3,6 +3,10 @@ output "vpc_id" {
   value = data.aws_vpc.custom.id
 }
 
+output "public_subnets" {
+  value = data.aws_subnets.public.ids
+}
+
 output "private_subnets" {
   value = data.aws_subnets.private.ids
 }
