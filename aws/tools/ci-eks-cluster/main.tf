5d457c3d27f632421e3cf79f61c436b90cd0bd89
Ben Rhine <rhine.ben@gmail.com>
2026-01-19 14:00:06 -0700

    adding readme

--- aws/tools/ci-eks-cluster/main.tf
+++ aws/tools/ci-eks-cluster/main.tf
@@ -64,17 +64,17 @@ resource "aws_iam_role_policy_attachment" "eks_policy" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
-#
-# resource "aws_eks_access_policy_association" "eks_access_policy" {
-#   cluster_name  = aws_eks_cluster.eks_cluster.name
-#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolic"
-#   principal_arn = aws_iam_role.eks_role.arn
-#
-#   access_scope {
-#     type       = "cluster"
-#   }
-#   depends_on = [aws_iam_role.eks_role, aws_eks_cluster.eks_cluster]
-# }
+
+resource "aws_eks_access_policy_association" "eks_access_policy" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_iam_role.eks_role.arn
+
+  access_scope {
+    type       = "cluster"
+  }
+  depends_on = [aws_iam_role.eks_role, aws_eks_cluster.eks_cluster]
+}
 
 resource "aws_iam_role" "eks_node_group_role" {
   name = "eks-node-group-role"
