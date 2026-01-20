e639b5a26eed53af4372361bcd5a4af4687386b0
Ben Rhine <rhine.ben@gmail.com>
2026-01-19 23:21:57 -0700

    adding readme

--- aws/tools/ci-kube-argo/main.tf
+++ aws/tools/ci-kube-argo/main.tf
@@ -5,69 +5,69 @@ resource "kubernetes_namespace" "terraform_argocd" {
   }
 }
 
-resource "kubernetes_deployment" "argocd_server" {
-  metadata {
-    name      = "argocd-server"
-    namespace = kubernetes_namespace.terraform_argocd.metadata[0].name
-    labels = {
-      app = "argocd-server"
-    }
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "argocd-server"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "argocd-server"
-        }
-      }
-
-      spec {
-        container {
-          name  = "argocd-server"
-          image = "quay.io/argoproj/argocd:v2.9.3"
-
-          args = [
-            "argocd-server",
-            "--insecure"
-          ]
-
-          port {
-            container_port = 8080
-          }
-
-          readiness_probe {
-            http_get {
-              path = "/healthz"
-              port = 8080
-            }
-            initial_delay_seconds = 10
-            period_seconds        = 10
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/healthz"
-              port = 8080
-            }
-            initial_delay_seconds = 30
-            period_seconds        = 20
-          }
-        }
-      }
-    }
-  }
-}
-
-
+# resource "kubernetes_deployment" "argocd_server" {
+#   metadata {
+#     name      = "argocd-server"
+#     namespace = kubernetes_namespace.terraform_argocd.metadata[0].name
+#     labels = {
+#       app = "argocd-server"
+#     }
+#   }
+#
+#   spec {
+#     replicas = 1
+#
+#     selector {
+#       match_labels = {
+#         app = "argocd-server"
+#       }
+#     }
+#
+#     template {
+#       metadata {
+#         labels = {
+#           app = "argocd-server"
+#         }
+#       }
+#
+#       spec {
+#         container {
+#           name  = "argocd-server"
+#           image = "quay.io/argoproj/argocd:v2.9.3"
+#
+#           args = [
+#             "argocd-server",
+#             "--insecure"
+#           ]
+#
+#           port {
+#             container_port = 8080
+#           }
+#
+#           readiness_probe {
+#             http_get {
+#               path = "/healthz"
+#               port = 8080
+#             }
+#             initial_delay_seconds = 10
+#             period_seconds        = 10
+#           }
+#
+#           liveness_probe {
+#             http_get {
+#               path = "/healthz"
+#               port = 8080
+#             }
+#             initial_delay_seconds = 30
+#             period_seconds        = 20
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+#
+#
 # resource "kubernetes_service" "argocd_server" {
 #   metadata {
 #     name      = "argocd-server"
@@ -85,6 +85,36 @@ resource "kubernetes_deployment" "argocd_server" {
 #       target_port = 8080
 #     }
 #
-#     type = "LoadBalancer"
+#     type = "NodePort"
 #   }
 # }
+
+resource "helm_release" "argocd" {
+  # depends_on = [aws_eks_node_group.main]
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "4.5.2"
+
+  namespace = "argocd"
+
+  create_namespace = true
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    value = "nlb"
+  }
+}
+
+
+data "kubernetes_service" "argocd_server" {
+  metadata {
+    name      = "argocd-server"
+    namespace = helm_release.argocd.namespace
+  }
+}
