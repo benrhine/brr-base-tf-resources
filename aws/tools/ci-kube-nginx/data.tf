9d786dea06c9abe3e45f5cb2ce6c19528710b8f4
Ben Rhine <rhine.ben@gmail.com>
2026-01-19 22:43:25 -0700

    adding readme

--- aws/tools/ci-kube-nginx/data.tf
+++ aws/tools/ci-kube-nginx/data.tf
@@ -4,11 +4,11 @@ data "aws_availability_zones" "available" {}
 
 
 data "aws_eks_cluster" "eks_cluster" {
-  name = "my-eks-cluster-example-1-trNUw3H4"
+  name = "my-eks-cluster-example-1-uJ2G6bFh"
 }
 
 data "aws_eks_cluster_auth" "eks_cluster" {
-  name = "my-eks-cluster-example-1-trNUw3H4"
+  name = "my-eks-cluster-example-1-uJ2G6bFh"
 }
 
 # -----------------------------
