2765075b8d9ba17793e6620db19f4621eda48330
Ben Rhine <rhine.ben@gmail.com>
2026-01-19 18:28:49 -0700

    adding readme

--- aws/tools/ci-kube-argo/data.tf
+++ aws/tools/ci-kube-argo/data.tf
@@ -4,9 +4,9 @@ data "aws_availability_zones" "available" {}
 
 
 data "aws_eks_cluster" "eks_cluster" {
-  name = "my-eks-cluster-example-1-twc9rNT9"
+  name = "my-eks-cluster-example-1-trNUw3H4"
 }
 
 data "aws_eks_cluster_auth" "eks_cluster" {
-  name = "my-eks-cluster-example-1-twc9rNT9"
+  name = "my-eks-cluster-example-1-trNUw3H4"
 }
