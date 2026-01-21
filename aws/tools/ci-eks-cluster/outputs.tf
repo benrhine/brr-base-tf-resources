c16ba4e0cb0df9e206991bdfa0b5a9e46e592e51
Ben Rhine <rhine.ben@gmail.com>
2026-01-21 12:57:30 -0700

    updating tf outputs

--- aws/tools/ci-eks-cluster/outputs.tf
+++ aws/tools/ci-eks-cluster/outputs.tf
@@ -10,3 +10,44 @@ output "public_subnets" {
 output "private_subnets" {
   value = data.aws_subnets.private.ids
 }
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
+
+output "eks_cluster_arn" {
+  value = aws_eks_cluster.eks_cluster.arn
+}
+
+output "eks_cluster_node_group_arn" {
+  value = aws_eks_node_group.eks_node_group.arn
+}
+
+output "eks_role_name" {
+  value = aws_iam_role.eks_role.name
+}
+
+output "eks_role_arn" {
+  value = aws_iam_role.eks_role.arn
+}
+
+output "eks_ng_role_name" {
+  value = aws_iam_role.eks_node_group_role.name
+}
+
+output "eks_ng_role_arn" {
+  value = aws_iam_role.eks_node_group_role.arn
+}
+
+output "cluster_admin_role_001" {
+  value = aws_eks_access_entry.eks_role_access.principal_arn
+}
+
+output "cluster_admin_role_002" {
+  value = aws_eks_access_entry.oidc_role_access.principal_arn
+}
+
+output "cluster_admin_role_003" {
+  value = aws_eks_access_entry.sso_role_access.principal_arn
+}
+
