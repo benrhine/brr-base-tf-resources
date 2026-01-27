32ba68a0a6adfa650859bfa837c6bcb6c76e158f
Ben Rhine <rhine.ben@gmail.com>
2026-01-27 13:47:21 -0700

    adding eks addon to warm cluster

--- aws/tools/ci-eks-std-module-managed-ng/main.tf
+++ aws/tools/ci-eks-std-module-managed-ng/main.tf
@@ -356,22 +356,22 @@ resource "aws_eks_access_policy_association" "eks_role_access_policy" {
   depends_on = [aws_iam_role.eks_role, module.eks_cluster]
 }
 
-resource "aws_eks_access_entry" "oidc_role_access" {
-  cluster_name            = module.eks_cluster.cluster_name
-  principal_arn           = local.ci_role_arn
-  type                    = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "oidc_role_access_policy" {
-  cluster_name            = module.eks_cluster.cluster_name
-  policy_arn              = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn           = local.ci_role_arn
-
-  access_scope {
-    type                  = "cluster"
-  }
-  depends_on = [aws_iam_role.eks_role, module.eks_cluster]
-}
+# resource "aws_eks_access_entry" "oidc_role_access" {
+#   cluster_name            = module.eks_cluster.cluster_name
+#   principal_arn           = local.ci_role_arn
+#   type                    = "STANDARD"
+# }
+#
+# resource "aws_eks_access_policy_association" "oidc_role_access_policy" {
+#   cluster_name            = module.eks_cluster.cluster_name
+#   policy_arn              = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#   principal_arn           = local.ci_role_arn
+#
+#   access_scope {
+#     type                  = "cluster"
+#   }
+#   depends_on = [aws_iam_role.eks_role, module.eks_cluster]
+# }
 
 resource "aws_eks_access_entry" "sso_role_access" {
   cluster_name            = module.eks_cluster.cluster_name
--- aws/tools/ci-kube-argo/main.tf
+++ aws/tools/ci-kube-argo/main.tf
@@ -1,3 +1,12 @@
+########################################################################################################################
+# Should resource names be prefixed with their environment? This is an interesting question around environment design.
+# Do you want a cluster per environment? e.g. dev-cluster, test-cluster ... or do you want something more like a cluster
+# per account where the cluster in the dev account may hold both dev and test deployments, the cluster in non-prod may
+# hold the uat and regression deployments.
+########################################################################################################################
+locals {
+  resource_prefix = var.environment_prefix ? "${var.environment}-${var.project_name}-${var.project_id}" : "${var.project_name}-${var.project_id}"
+}
 
 resource "kubernetes_namespace" "terraform_argocd" {
   metadata {
